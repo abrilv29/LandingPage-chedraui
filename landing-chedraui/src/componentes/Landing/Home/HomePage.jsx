781d1c455e5c16235d1831fdd89383c98d4191e6
Abril <abrilv29@gmail.com>
2024-01-20 23:38:27 -0600

    Componente compras en linea

--- landing-chedraui/src/componentes/Landing/Home/HomePage.jsx
+++ landing-chedraui/src/componentes/Landing/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import ProductAll from "../../Productos/ProductAll"
 import CartCagory from "../CartCagory"
 import Newsletter from "./Newsletter"
 import Footer from "./Footer"
+import Online from "../Online"
 
 
 function HomePage() {
@@ -9,6 +10,7 @@ function HomePage() {
     <>
       <CartCagory />
       <ProductAll />
+      <Online/>
       <Newsletter/>
       <Footer/>
     </>
